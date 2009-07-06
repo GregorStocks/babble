824f0a8a8966974f2d4a59490b452520091548f2
Kurper <kurper@gmail.com>
2009-07-06 19:46:01 -0400

    initial commit. currently you can drag things around and put them in a sentence box, but it doesn't parse them or anything, just spits them all back out again in the sentence <p>.

---
+++ amalgam.js
@@ -0,0 +1,65 @@
+var words = ["butts",  "bums", "buttes", "frankly i am insulted by the preceding terms", "butt", "-y", "-s", "hurf", ",", "!", "."];
+
+function updateSentence() {
+	$('#sentence').empty();
+	$.each($('#dropbox > .wordbox'), function(idx, box) {
+		$('#sentence').append($(box).text() + " ");
+	});
+}
+
+$(function(){
+	$('#dropbox').bind('drop', function(event) {
+		// find the farthest-left wordbox with a center to the right of the mouse pointer.
+		// insert word before it.
+		// note that the wordboxes are already sorted in order for us.
+		var done = false;
+		$.each($('#dropbox > .wordbox'), function(idx, box) {
+			if(!done && $(box).position()['left'] + $(box).width() / 2 > event.pageX) {
+				done = true;
+				if(box != event.dragTarget) {
+					// can't insert it before itself, just don't do anything instead
+					$(event.dragTarget).insertBefore(box);
+				}
+			}
+		});
+		if(!done) {
+			$(this).append(event.dragTarget);
+		}
+		$(event.dragTarget).css({
+			position: 'static',
+		});
+		updateSentence();
+	});
+
+	// first, insert all the boxes with static positioning
+	for(var i = 0; i < words.length; i++) {
+		var box = $('<span id="wordbox' + i + '" class="wordbox">' + words[i] + '</span>');
+		$('#wordsbox').append(box);
+	}
+
+	// next, save all of their positions
+	var box_poses = []
+	$.each($('.wordbox'), function(idx, box) {
+		box_poses[$(box).attr('id')] = $(box).position(); 
+	});
+
+	// then, convert them all to absolute positioning using saved positions
+	$.each($('.wordbox'), function(idx, box) {
+		$(box).css({
+			position: 'absolute',
+			top: box_poses[$(box).attr('id')]['top'],
+			left: box_poses[$(box).attr('id')]['left']
+		});
+	});
+	$('.wordbox').bind('drag', function(event){
+		$(this).css({
+			position: 'absolute',
+			top: event.offsetY,
+			left: event.offsetX
+		});
+	})
+	.bind('dragstart', function(event) {
+		$('body').append(this); // remove from the sentence
+		updateSentence();
+	});
+});
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Amalgam</title>
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script src="jquery-1.3.2.js" type="text/javascript"></script>
+	<script src="jquery.event.drag-1.5.js" type="text/javascript"></script>
+	<script src="jquery.event.drop-1.2.js" type="text/javascript"></script>
+	<script src="amalgam.js" type="text/javascript"></script>
+</head>
+<body>
+<div class="wordsbox wordcontainer" id="wordsbox"></div>
+<div class="dropbox wordcontainer" id="dropbox"></div>
+<p class="sentence" id="sentence"></p>
+<p id="log"></p>
+</body>
+</html>
