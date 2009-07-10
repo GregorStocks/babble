5721566dbc655d78fca959ff245410139d7351ef
Kurper <kurper@gmail.com>
2009-07-10 14:08:03 -0400

    Wordlists are now randomly generated on each page load, with about 20 words guaranteed to show up and the rest chosen randomly (with repeats) from all words, including the guaranteed ones.

--- api/geteventssince.cgi
+++ api/geteventssince.cgi
@@ -9,17 +9,27 @@ if(form.has_key("eventnum")):
 	eventnum = form["eventnum"].value
 	# TODO: return all events since that one.
 
-import MySQLdb, json
+import MySQLdb, json, random
 
 db = MySQLdb.connect("localhost", "username", "password", "amalgam")
 cursor = db.cursor(MySQLdb.cursors.DictCursor)
-cursor.execute("SELECT word FROM words")
+cursor.execute("SELECT word, minnum FROM words")
 rows = cursor.fetchall()
 cursor.close()
+
+words = []
+for row in rows:
+	for i in range(row["minnum"]):
+		words.append(row["word"])
+
+numwords = 60
+
+for i in range(numwords - len(words)):
+	words.append(random.choice(rows)["word"])
 
 print "Content-type: text/plain;charset=utf-8"
 # theoretically this should be served as application/json, but firefox opens up
 # a "save" box for that instead of displaying it inline, which is inconvenient
 # for debugging. 
 print
-print json.dumps([{'type': 'words', 'words': [row["word"] for row in rows]}], indent=4)
+print json.dumps([{'type': 'words', 'words': sorted(words, key=str.lower)}], indent=4)
--- doc/setup.sql
+++ doc/setup.sql
@@ -1,24 +1,24 @@
 CREATE TABLE IF NOT EXISTS rounds (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY  (id));
-CREATE TABLE IF NOT EXISTS words (id INT NOT NULL AUTO_INCREMENT, word TEXT NOT NULL, PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS words (id INT NOT NULL AUTO_INCREMENT, word TEXT NOT NULL, minnum INT NOT NULL DEFAULT 0, PRIMARY KEY (id));
 CREATE TABLE IF NOT EXISTS roundwords (id INT NOT NULL AUTO_INCREMENT, roundid INT NOT NULL, wordid INT NOT NULL,  PRIMARY KEY (id));
 
 -- add the default wordlist
-INSERT INTO words (word) VALUES
-('I'), ('you'), ('he'), ('she'), ('they'), ('it'), ('we'),
-('is'), ('are'), ('eat'), ('kill'), ('cuss'),
-('butt'), ('human'), ('pair'),
-('groggy'), ('ugly'), ('dumb'), ('fat'), ('phat'),
-('-s'), ('-y'), ('-ly'), ('-ize'), ('-ify'), ('-ed'),
-('pro-'), ('anti-'),
-('nine'), ('four'), ('six'), ('eighty'),
-('ain''t'),
-('myself'), ('yourself'), ('herself'),
-('my'), ('your'), ('her'), ('his'),
-('other'),
-('who'),
-('through'), ('on'), ('top'), ('of'), ('with'), ('for'), ('to'), ('from'), ('until'),
-('the'), ('even'), ('so'), ('well'),
-('still'), ('yet'), ('also'), ('and'), ('too'), ('but'), ('while'),
-('a'), ('a'), ('the'), ('any'), ('many'),
-('!'), ('!'), ('.'), ('.'), ('?'),
-(','), (';'), (':');
+INSERT INTO words (word, minnum) VALUES
+('I', 1), ('you', 1), ('he', 0), ('she', 0), ('they', 0), ('it', 0), ('we', 0),
+('is', 1), ('are', 1), ('eat', 0), ('kill', 0), ('cuss', 0),
+('butt', 0), ('human', 0), ('pair', 0),
+('groggy', 0), ('ugly', 0), ('dumb', 0), ('fat', 0), ('phat', 0),
+('-s', 2), ('-y', 1), ('-ly', 1), ('-ize', 0), ('-ify', 0), ('-ed', 1),
+('pro-', 0), ('anti-', 0),
+('nine', 0), ('four', 0), ('six', 0), ('eighty', 0),
+('ain''t', 0),
+('myself', 0), ('yourself', 0), ('herself', 0),
+('my', 0), ('your', 0), ('her', 0), ('his', 0),
+('other', 0),
+('who', 0),
+('through', 0), ('on', 0), ('top', 0), ('of', 0), ('with', 0), ('for', 0), ('to', 0), ('from', 0), ('until', 0),
+('even', 0), ('so', 0), ('well', 0),
+('still', 0), ('yet', 0), ('also', 0), ('and', 0), ('too', 0), ('but', 0), ('while', 0),
+('a', 2), ('the', 1), ('any', 0), ('many', 0),
+('!', 1), ('.', 1), ('?', 1),
+(',', 1), (';', 0), (':', 0);
