bc504f52f26201614fb7407d94aa41f705ec5d59
Kurper <kurper@gmail.com>
2009-08-24 14:10:56 -0400

    ok connection pooling achieved nothing, it looks like python is just doomed to be slow on NFS's servers

--- api/lib/SQL.py
+++ api/lib/SQL.py
@@ -2,13 +2,10 @@ from __future__ import absolute_import
 
 import MySQLdb
 from .const import config
-import sqlalchemy.pool as pool
-
-mysql = pool.manage(MySQLdb)
 
 def get_conn():
-	return mysql.connect(config.SQL_NAME, config.SQL_USERNAME,
+	return MySQLdb.connect(config.SQL_NAME, config.SQL_USERNAME,
 		config.SQL_PASSWORD, config.SQL_DATABASE)
 
 def get_cursor(conn):
-	return conn.cursor(mysql.cursors.DictCursor)
+	return conn.cursor(MySQLdb.cursors.DictCursor)
