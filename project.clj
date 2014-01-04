210a59f76adf40b48e1722650eff4fc1cd654ab2
GregorStocks <gregor@levelmoney.com>
2014-01-03 22:34:38 -0800

    add cljs project

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject cljsbuild-example-simple "1.0.1"
+  :description "A simple example of how to use lein-cljsbuild"
+  :source-paths ["src-clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2014"
+                  :exclusions [org.apache.ant/ant]]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.0.1"]
+            [lein-ring "0.8.7"]]
+  :cljsbuild {
+    :builds [{:source-paths ["src-cljs"]
+              :compiler {:output-to "resources/public/js/main.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}]}
+  :ring {:handler example.routes/app})
